f036340fa895376caca4e08cd6eef28341721abc
Exon2007 <lelongtristan474@gmail.com>
2026-01-03 12:35:34 +0100

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,22 +1,30 @@
 (() => {
-  const iframe = document.createElement("iframe");
-  iframe.src = location.origin;
-  iframe.style.width = "300px";
-  iframe.style.height = "150px";
-  document.body.appendChild(iframe);
+  const wait = setInterval(() => {
+    if (!document.body) return;
 
-  iframe.onload = () => {
-    console.log("iframe loaded");
-    try {
-      const doc = iframe.contentDocument;
-      console.log("contentDocument accessible:", !!doc);
-      console.log("iframe origin:", iframe.contentWindow.location.origin);
-      console.log("parent origin:", location.origin);
-      console.log("same origin:", iframe.contentWindow.location.origin === location.origin);
-      console.log("iframe body:", doc.body);
-      console.log("iframe body innerHTML length:", doc.body.innerHTML.length);
-    } catch (e) {
-      console.log("DOM access blocked:", e.toString());
-    }
-  };
+    clearInterval(wait);
+
+    const iframe = document.createElement("iframe");
+    iframe.src = location.origin;
+    iframe.style.width = "300px";
+    iframe.style.height = "150px";
+
+    document.body.appendChild(iframe);
+    console.log("iframe appended");
+
+    iframe.onload = () => {
+      console.log("iframe loaded");
+      try {
+        const doc = iframe.contentDocument;
+        console.log("contentDocument accessible:", !!doc);
+        console.log("iframe origin:", iframe.contentWindow.location.origin);
+        console.log("parent origin:", location.origin);
+        console.log("same origin:", iframe.contentWindow.location.origin === location.origin);
+        console.log("iframe body:", doc.body);
+        console.log("iframe body innerHTML length:", doc.body ? doc.body.innerHTML.length : null);
+      } catch (e) {
+        console.log("DOM access blocked:", e.toString());
+      }
+    };
+  }, 50);
 })();
