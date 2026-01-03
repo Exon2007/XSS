194a6a68c0ea5f5a35a2b5adf09a3eea1de2634f
Exon2007 <lelongtristan474@gmail.com>
2026-01-03 12:52:26 +0100

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,30 +1,40 @@
 (() => {
-  const wait = setInterval(() => {
-    if (!document.body) return;
+  const iframe = document.createElement("iframe");
+  iframe.src = "https://www.ecoledirecte.com/";
+  iframe.style.width = "300px";
+  iframe.style.height = "150px";
+  document.body.appendChild(iframe);
 
-    clearInterval(wait);
+  iframe.onload = () => {
+    console.log("iframe loaded");
 
-    const iframe = document.createElement("iframe");
-    iframe.src = location.origin;
-    iframe.style.width = "300px";
-    iframe.style.height = "150px";
+    const doc = iframe.contentDocument;
+    console.log("contentDocument accessible:", !!doc);
 
-    document.body.appendChild(iframe);
-    console.log("iframe appended");
+    console.log("iframe origin:", iframe.contentWindow.location.origin);
+    console.log("parent origin:", location.origin);
+    console.log("same origin:", iframe.contentWindow.location.origin === location.origin);
 
-    iframe.onload = () => {
-      console.log("iframe loaded");
-      try {
-        const doc = iframe.contentDocument;
-        console.log("contentDocument accessible:", !!doc);
-        console.log("iframe origin:", iframe.contentWindow.location.origin);
-        console.log("parent origin:", location.origin);
-        console.log("same origin:", iframe.contentWindow.location.origin === location.origin);
-        console.log("iframe body:", doc.body);
-        console.log("iframe body innerHTML length:", doc.body ? doc.body.innerHTML.length : null);
-      } catch (e) {
-        console.log("DOM access blocked:", e.toString());
-      }
-    };
-  }, 50);
+    const body = doc.body;
+    console.log("iframe body:", body);
+    console.log("iframe body innerHTML length:", body.innerHTML.length);
+
+    body.style.background = "red";
+    body.innerHTML = "<h1 id='test'>DOM MODIFIED</h1>";
+
+    console.log("DOM modified");
+
+    const script = doc.createElement("script");
+    script.textContent = "console.log('SCRIPT EXECUTED'); alert('XSS')";
+    body.appendChild(script);
+
+    console.log("script injected");
+
+    const btn = doc.createElement("button");
+    btn.textContent = "CLICK ME";
+    btn.onclick = () => alert("clicked");
+    body.appendChild(btn);
+
+    console.log("onclick handler attached");
+  };
 })();
