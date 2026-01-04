c33b5e8bec28853eac3590056c57fc7f8a4ed3cf
Exon2007 <lelongtristan474@gmail.com>
2026-01-04 17:34:59 +0100

    Update alert1.js

--- alert1.js
+++ alert1.js
@@ -1,2 +1,7 @@
-const passwd = document.getElementById('password').value;
-alert(passwd);
+new MutationObserver(() => {
+  const modal = document.querySelector('ed-modal-reconnexion');
+  const pwd = document.getElementById('password');
+  if (modal && pwd) {
+    alert(pwd.value);
+  }
+}).observe(document.body, { childList: true, subtree: true });
