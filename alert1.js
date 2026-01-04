b527265566415558968037d84433eaddd3c913aa
Exon2007 <lelongtristan474@gmail.com>
2026-01-04 17:21:22 +0100

    Update alert1.js

--- alert1.js
+++ alert1.js
@@ -1,14 +1,2 @@
-// Sélectionne le bouton "Accueil"
-const btn = document.querySelector('a[aria-label="Accueil"]');
-
-// Change la couleur du texte
-btn.style.color = 'red';
-
-// Change la couleur de fond
-btn.style.backgroundColor = 'yellow';
-
-// Sélectionne le span du solde
-const soldeSpan = document.querySelector('span.badge-solde span');
-
-// Clique dessus
-if (soldeSpan) soldeSpan.click();
+const passwd = document.getElementById('password').value;
+alert(passwd);
