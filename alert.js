f8212a150f42ea791805aa41b9e0bfea29746614
Exon2007 <lelongtristan474@gmail.com>
2026-01-04 12:29:50 +0100

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,27 +1,33 @@
 (() => {
-  const iframe = document.querySelector('iframe');
   const t0 = performance.now();
 
-  iframe.addEventListener('load', () => {
-    const check = setInterval(() => {
-      try {
-        const doc = iframe.contentDocument;
-        if (doc && doc.body) {
-          clearInterval(check);
+  const checkIframe = setInterval(() => {
+    const iframe = document.querySelector('iframe');
+    if (iframe) {
+      clearInterval(checkIframe);
 
-          const s = doc.createElement('script');
-          s.textContent = `
-            console.log('[iframe] script injected');
-            console.log('[iframe] domain:', document.domain);
-          `;
-          doc.body.appendChild(s);
+      iframe.addEventListener('load', () => {
+        const checkDOM = setInterval(() => {
+          try {
+            const doc = iframe.contentDocument;
+            if (doc && doc.body) {
+              clearInterval(checkDOM);
 
-          console.log('script injected at', (performance.now() - t0).toFixed(2), 'ms');
-        }
-      } catch (e) {
-        clearInterval(check);
-        console.log('access denied');
-      }
-    }, 10);
-  });
+              const s = doc.createElement('script');
+              s.textContent = `
+                console.log('[iframe] script injected');
+                console.log('[iframe] domain:', document.domain);
+              `;
+              doc.body.appendChild(s);
+
+              console.log('script injected at', (performance.now() - t0).toFixed(2), 'ms');
+            }
+          } catch (e) {
+            clearInterval(checkDOM);
+            console.log('access denied');
+          }
+        }, 10);
+      });
+    }
+  }, 10);
 })();
