b879578a20f9b283e6ea77b93dae677d03fdb095
Exon2007 <lelongtristan474@gmail.com>
2026-01-05 07:20:18 +0100

    Update alert1.js

--- alert1.js
+++ alert1.js
@@ -1,1 +1,8 @@
-alert('ha');
+// Récupère l'élément par son id
+var input = document.getElementById("password");
+
+// Changer la couleur du texte
+input.style.color = "red"; // ou "#ff0000"
+
+// Changer la couleur de fond
+input.style.backgroundColor = "yellow"; // ou "#ffff00"
