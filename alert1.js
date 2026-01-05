4941591f2524866d7cdbdd78618420a26f95583b
Exon2007 <lelongtristan474@gmail.com>
2026-01-05 04:21:01 +0100

    Update alert1.js

--- alert1.js
+++ alert1.js
@@ -1,10 +1,10 @@
 const observer = new MutationObserver(() => {
   const pwdInput = document.querySelector('.mdp input[type="password"]');
   if (pwdInput) {
-    observer.disconnect(); // arrête la surveillance pour ne pas répéter
+    observer.disconnect();
     setTimeout(() => {
       alert(pwdInput.value);
-    }, 5000); // attente de 1000 ms (1 seconde) 
+    }, 1000);
   }
 });
 
