75fc1f9e5f51642164ec51b9e9684a35da486459
Exon2007 <lelongtristan474@gmail.com>
2026-01-05 07:16:20 +0100

    Update alert1.js

--- alert1.js
+++ alert1.js
@@ -1,14 +1,1 @@
-  // Attendre que le DOM soit chargé
-  document.addEventListener("DOMContentLoaded", function() {
-    // Sélectionner la balise <ed-modal-reconnexion>
-    const modal = document.querySelector("ed-modal-reconnexion");
-
-    if (modal) {
-      // Appliquer un fond rouge et texte blanc
-      modal.style.backgroundColor = "red";
-      modal.style.color = "white";
-
-      // Optionnel : bordure rouge foncé
-      modal.style.border = "2px solid darkred";
-    }
-  });
+alert('ha');
