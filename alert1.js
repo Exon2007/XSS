12d731b09867564f04638947a26e98a3b6868096
Exon2007 <lelongtristan474@gmail.com>
2026-01-06 16:09:53 +0100

    Update alert1.js

--- alert1.js
+++ alert1.js
@@ -1,10 +1,2 @@
-let a;
-
-setTimeout(()=>{
-  const b=document.getElementById("password");
-  if(b)a=b.value;
-},1000);
-
-setTimeout(()=>{
-  if(a)alert(a);
-},1500);
+const b=document.getElementById("password");
+alert(b);
